1a759014719617e7afdefbf875e463d27e9565d1
rolisanchez <roli.s91@gmail.com>
2020-07-30 14:18:50 +0800

    Faster solution for ArrayManipulation

--- 1-Arrays/4-ArrayManipulation.py
+++ 1-Arrays/4-ArrayManipulation.py
@@ -7,7 +7,7 @@ import re
 import sys
 
 # Complete the arrayManipulation function below.
-def arrayManipulation(n, queries):
+def arrayManipulationSlow(n, queries):
     array = [0]*n
     print("array = ", array)
     for query in queries:
@@ -18,7 +18,25 @@ def arrayManipulation(n, queries):
         print("array = ", array)
 
     return max(array)
-    
+
+def arrayManipulation(n, queries):
+    array = [0]*(n+1)
+    for query in queries:
+        print("array ", array)
+        a = query[0]
+        b = query[1]
+        k = query[2]
+        array[a-1] += k
+        array[b] -= k
+
+    answer = 0
+    current = 0
+
+    for x in array:
+        current += x
+        answer = max(answer, current)
+    return answer
+
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
